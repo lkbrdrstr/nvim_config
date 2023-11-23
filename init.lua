6d90a3708135e7ae7285fecb13051aa503050092
Loki Oberdörster <loki.oberdoerster@icloud.com>
2023-11-23 15:40:24 +0100

    change luasnip jump triggers

--- init.lua
+++ init.lua
@@ -4,13 +4,8 @@ require "config.keymaps"
 require "config.lazy"
 
 vim.cmd [[
-" Use Tab to expand and jump through snippets
 imap <silent><expr> jk luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : 'jk'
 smap <silent><expr> jk luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : 'jk'
-
-" Use Shift-Tab to jump backwards through snippets
-smap <silent><expr> kj luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : 'kj'
-smap <silent><expr> kj luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : 'kj'
 ]]
 
 require "luasnip.loaders.from_lua".load { paths = "~/.config/nvim/LuaSnip" }
