7a9cb3a1741bdfb9e871f31c57f2c05ce305683e
Loki Oberdörster <loki.oberdoerster@icloud.com>
2023-11-29 21:35:11 +0100

    populate ensure_installed for treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,17 +1,35 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        local configs = require "nvim-treesitter.configs"
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local configs = require "nvim-treesitter.configs"
 
-        configs.setup {
-            ensure_installed = {},
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
-            incremental_selection = { enable = true },
-            indent = { enable = true },
-        }
-    end,
+    configs.setup {
+      ensure_installed = {
+        "bash",
+        "c",
+        "cpp",
+        "csv",
+        "dockerfile",
+        "fish",
+        "go",
+        "haskell",
+        "latex",
+        "lua",
+        "make",
+        "markdown",
+        "rust",
+        "swift",
+        "toml",
+        "vim",
+        "vimdoc",
+      },
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+      incremental_selection = { enable = true },
+      indent = { enable = true },
+    }
+  end,
 }
